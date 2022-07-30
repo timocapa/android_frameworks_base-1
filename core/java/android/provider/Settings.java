AOSPA/android_frameworks_base-old

SystemUI: Allow toggling screen off UDFPS [1/2]
Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
Change-Id: I5c337c1090ceae14d86f92a3cde67792133e18a2

core/java/android/provider/Settings.java
@@ -5220,6 +5220,13 @@ public final class Settings {
         public static final String ALERT_SLIDER_ORDER = "alert_slider_order";
 
         /**
+         * Enable fod detection even when screen is off
+         * Default value is 0
+         * @hide
+         */
+        public static final String SCREEN_OFF_FOD = "screen_off_fod";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate

timocapa/android_frameworks_base-1

Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
Change-Id: Ic4bfc8fcc275a030f0d5d968aea7e899653ea783

core/java/android/provider/Settings.java
@@ -10368,6 +10368,13 @@ public final class Settings {
         public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
 
         /**
+         * Enable udfps detection even when screen is off
+         * Default value is 0
+         * @hide
+         */
+        public static final String SCREEN_OFF_UDFPS_ENABLED = "screen_off_udfps_enabled";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *